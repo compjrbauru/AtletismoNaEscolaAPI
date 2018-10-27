a45a7bb0a22d3f923e7092a6cf8be4316aa77cb9
Tonhao165 <eugenio165.as@gmail.com>
2018-10-27 16:14:24 -0300

    161519919: Criando endpoint para pegar professores e arrumando bugs

--- api/controllers/AccountController.js
+++ api/controllers/AccountController.js
@@ -8,37 +8,43 @@
 module.exports = {
 
   signup: async function (inputs, res) {
+    if (inputs.session.User === undefined || inputs.session.User.role == 'aluno')
+      return res.badRequest('ACESSO RESTRITO');
     var newEmailAddress = inputs.body.emailAddress.toLowerCase();
 
     // Build up data for the new user record and save it to the database.
     // (Also use `fetch` to retrieve the new ID so that we can use it below.)
     var newUserRecord = await Account.create(Object.assign({
+      ...inputs.body,
       emailAddress: newEmailAddress,
       password: await sails.helpers.passwords.hashPassword(inputs.body.password),
-      fullName: inputs.body.fullName,
-      escola: inputs.body.escola,
-      ano: inputs.body.ano,
-    }))
-    .intercept('E_UNIQUE', 'emailAlreadyInUse')
-    .intercept({name: 'UsageError'}, 'invalid')
-    .fetch();
+    })).fetch();
 
-    return res.ok();
+    return res.status(200).json(newUserRecord);
 
+  },
+
+  getProfessores: async function (req, res) { // Retorna todos os professores para superadmin e todos os professores do colegio para diretor
+    let response;
+    if (req.session.User === undefined || req.session.User.role in ['aluno', 'professor'])
+      return res.badRequest('ACESSO RESTRITO');
+    else if (req.session.User.role === 'superadmin')
+      response = await Account.find({
+        role: 'professor',
+      }).populate('escola');
+    else if (req.session.User.role === 'diretor')
+      response = await Account.find({
+        role: 'professor',
+        escola: req.session.User.escola.id,
+      }).populate('escola');
+
+    return res.status(200).json(response);
   },
 
   getAccount: async function (req, res) {
     let id = req.param('id');
-    let response = (id) ? await Account.findOne({id: id}) : await Account.find();
+    let response = (id) ? await Account.findOne({id: id}).populate('escola') : await Account.find().populate('escola');
     return res.status(200).json(response);
-  },
-
-  createAccount: async function (req, res) {
-    if (req.session.User === undefined || req.session.User.role == 'aluno')
-      return res.badRequest('ACESSO RESTRITO');
-
-    await Account.create(req.body);
-    return res.status(200).json('ok');
   },
 
   deleteAccount: async function(req, res) {
@@ -56,8 +62,11 @@ module.exports = {
       return res.badRequest('ACESSO RESTRITO');
 
     let id = req.param('id');
-    await Account.update({id: id}).set(req.body);
-    let record = await Account.findOne({id: id});
+    let newOBJ = {...req.body};
+    if (req.body.password)
+      newOBJ['password'] = await sails.helpers.passwords.hashPassword(req.body.password);
+    await Account.update({id: id}).set(newOBJ);
+    let record = await Account.findOne({id: id}).populate('escola');
     return res.status(200).json(record);
   }
 
